ceeed24e39a4d60cceaf5fa762d356878d1b7186
Meisie Widuri <dotkit@electrowizard.me>
2024-12-19 10:02:00 +0700

    X00TD: We are Matrixx

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -7,10 +7,26 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_DEVICE_AS_WEBCAM := false
+TARGET_ENABLE_BLUR := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Matrixx
+MATRIXX_MAINTAINER := ∂σткιт
+WITH_GMS := false
+
+# Extra Matrixx Stuff
+MATRIXX_CHIPSET := SDM660
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1080x2160
+BUILD_GOOGLE_CONTACTS := false
+BUILD_GOOGLE_DIALER := false
+BUILD_GOOGLE_MESSAGE := false
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
